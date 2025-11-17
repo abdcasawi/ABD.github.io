4d28ab48733754b5b168d155bc4dd14491dbaec3
abdcasawi <abdcasawi2023@gmail.com>
2025-11-17 19:34:52 +0100

    Updated apps.ts

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -100,11 +100,12 @@ export const appCategories: Category[] = [
       {
         id: 'ostora',
         name: 'ostora',
-        icon: '📺',
+        icon: 'https://i.ibb.co/S768CDRq/ostora.png',
         description: 'Advanced IPTV player with VOD, series, and live TV support',
         rating: 4.7,
         downloads: '5M+',
         features: ['livetv','VOD', 'Series', 'EPG'],
+        downlladcode: '616308'
         downloadUrl: 'https://drive.usercontent.google.com/download?id=1snE_nqsCRY9jTrimsmhK56Vh7EzX4k0h&export=download&authuser=0&confirm=t&uuid=66970ed0-191a-4993-9277-3d212747bef2&at=ALWLOp6NU1Da5bQrCO4qFKoUuHY1%3A1763404174836',
       },
       {
