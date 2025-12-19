cf95f523f4e8f5fa54dbd3fbcefa51849df06bdb
abdcasawi <abdcasawi2023@gmail.com>
2025-12-19 22:19:06 +0100

    Updated apps.ts

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -26,7 +26,7 @@ export const appCategories: Category[] = [
         rating: 9.1,
         features: ['HD Streaming', 'Subtitles', 'No Ads'],
         downloadUrl: 'https://drive.usercontent.google.com/download?id=1DzRlhy5ADi_aHu3EqMC8ulZvAmwer8GU&export=download&authuser=0&confirm=t&uuid=d2cb6d45-3eb3-4c01-b9ab-b77cf872916c&at=ANTm3cxbRtDky7YOwPnUeitTMRxK%3A1766175192412',
-          downloadCode:'https://drive.google.com/file/d/1mSwPjL0ZY_QKEo4ufYtLJ1Bzqjr9ihyB/view',
+          downloadCode:downloadUrl:'https://drive.google.com/file/d/1mSwPjL0ZY_QKEo4ufYtLJ1Bzqjr9ihyB/view',
       },
       {
         id: 'DOWNLOADER',
